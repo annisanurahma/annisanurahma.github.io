e37fa60298d32712cdd586c60d328f64b71ba0d2
annisanurahma <annisanurahmah59@gmail.com>
2025-08-11 00:40:19 +0700

    edit banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/annisa_animasi.png";
+// import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -56,8 +57,9 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm Annisa`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "UI/UX Designer", "Web Designer", "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Web developer & tech enthusiast eager to learn and grow.
+                     Final year student at Multimedia Nusantara University, passionate about building meaningful digital experiences.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
